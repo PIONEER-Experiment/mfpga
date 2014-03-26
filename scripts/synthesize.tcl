7663d781bd00700fe12085c050805dc96dddde86
Nic Eggert <nse23@cornell.edu>
2014-03-26 13:37:14 -0400

    Update synthesize script

--- scripts/synthesize.tcl
+++ scripts/synthesize.tcl
@@ -1,8 +1,12 @@
 source setup.tcl
+
 source read_hdl.tcl
 source read_ip.tcl
+# source read_constraints.tcl
+read_xdc $ROOT/constraints/timing.xdc
 
-synth_design -top ipbus_only_top -part xc7k160tfbg676-2
-report_utilization
-report_timing
+synth_design -top ipbus_only_top -name ipbus_only -part xc7k160tfbg676-2
+refresh_design
+read_xdc $ROOT/constraints/ipbus_only_place.xdc
+
 start_gui
