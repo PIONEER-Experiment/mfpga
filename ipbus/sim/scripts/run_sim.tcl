9305027ff2b1ce415bd6796b37bf8b22bfeddd17
Nic Eggert <nse23@cornell.edu>
2014-03-26 13:28:34 -0400

    Add a IPbus simulation files

---
+++ ipbus/sim/scripts/run_sim.tcl
@@ -0,0 +1,11 @@
+source ../../../scripts/setup.tcl
+
+create_project -force ipbus_sim $ROOT/ipbus/project/
+
+source $ROOT/ipbus/read_hdl.tcl
+
+read_vhdl [list $ROOT/ipbus/sim/hdl/ipbus_tb.vhd $ROOT/ipbus/sim/hdl/package_ipbus_simulation.vhd]
+set_property top ipbus_tb [get_filesets sim_1]
+set_property runtime 5us [get_filesets sim_1]
+
+launch_xsim -mode behavioral
