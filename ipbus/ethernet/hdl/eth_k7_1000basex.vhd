b0cc1ee5f8c618a7d45a872c1dd94a1310cb1250
eggert <nse23@cornell.edu>
2014-03-28 15:16:05 -0400

    Add debug gtx ports for GigE to reverse tx and rx polarity

--- ipbus/ethernet/hdl/eth_k7_1000basex.vhd
+++ ipbus/ethernet/hdl/eth_k7_1000basex.vhd
@@ -67,41 +67,86 @@ architecture rtl of eth_k7_1000basex is
 	END COMPONENT;
 	
 	COMPONENT gig_ethernet_pcs_pma_0
-  PORT (
-    gtrefclk_p : IN STD_LOGIC;
-    gtrefclk_n : IN STD_LOGIC;
-    gtrefclk_out : OUT STD_LOGIC;
-    txn : OUT STD_LOGIC;
-    txp : OUT STD_LOGIC;
-    rxn : IN STD_LOGIC;
-    rxp : IN STD_LOGIC;
-    independent_clock_bufg : IN STD_LOGIC;
-    userclk_out : OUT STD_LOGIC;
-    userclk2_out : OUT STD_LOGIC;
-    rxuserclk_out : OUT STD_LOGIC;
-    rxuserclk2_out : OUT STD_LOGIC;
-    resetdone : OUT STD_LOGIC;
-    pma_reset_out : OUT STD_LOGIC;
-    mmcm_locked_out : OUT STD_LOGIC;
-    gmii_txd : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
-    gmii_tx_en : IN STD_LOGIC;
-    gmii_tx_er : IN STD_LOGIC;
-    gmii_rxd : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
-    gmii_rx_dv : OUT STD_LOGIC;
-    gmii_rx_er : OUT STD_LOGIC;
-    gmii_isolate : OUT STD_LOGIC;
-    configuration_vector : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
-    status_vector : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
-    reset : IN STD_LOGIC;
-    signal_detect : IN STD_LOGIC;
-    gt0_qplloutclk_out : OUT STD_LOGIC;
-    gt0_qplloutrefclk_out : OUT STD_LOGIC
-  );
-END COMPONENT;
---ATTRIBUTE SYN_BLACK_BOX : BOOLEAN;
---ATTRIBUTE SYN_BLACK_BOX OF gig_ethernet_pcs_pma_0 : COMPONENT IS TRUE;
---ATTRIBUTE BLACK_BOX_PAD_PIN : STRING;
---ATTRIBUTE BLACK_BOX_PAD_PIN OF gig_ethernet_pcs_pma_0 : COMPONENT IS "gtrefclk_p,gtrefclk_n,gtrefclk_out,txn,txp,rxn,rxp,independent_clock_bufg,userclk_out,userclk2_out,rxuserclk_out,rxuserclk2_out,resetdone,pma_reset_out,mmcm_locked_out,gmii_txd[7:0],gmii_tx_en,gmii_tx_er,gmii_rxd[7:0],gmii_rx_dv,gmii_rx_er,gmii_isolate,configuration_vector[4:0],status_vector[15:0],reset,signal_detect,gt0_qplloutclk_out,gt0_qplloutrefclk_out";
+	  PORT (
+	    gtrefclk_p : IN STD_LOGIC;
+	    gtrefclk_n : IN STD_LOGIC;
+	    gtrefclk_out : OUT STD_LOGIC;
+	    txn : OUT STD_LOGIC;
+	    txp : OUT STD_LOGIC;
+	    rxn : IN STD_LOGIC;
+	    rxp : IN STD_LOGIC;
+	    independent_clock_bufg : IN STD_LOGIC;
+	    userclk_out : OUT STD_LOGIC;
+	    userclk2_out : OUT STD_LOGIC;
+	    rxuserclk_out : OUT STD_LOGIC;
+	    rxuserclk2_out : OUT STD_LOGIC;
+	    resetdone : OUT STD_LOGIC;
+	    pma_reset_out : OUT STD_LOGIC;
+	    mmcm_locked_out : OUT STD_LOGIC;
+	    gmii_txd : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+	    gmii_tx_en : IN STD_LOGIC;
+	    gmii_tx_er : IN STD_LOGIC;
+	    gmii_rxd : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+	    gmii_rx_dv : OUT STD_LOGIC;
+	    gmii_rx_er : OUT STD_LOGIC;
+	    gmii_isolate : OUT STD_LOGIC;
+	    configuration_vector : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+	    status_vector : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+	    reset : IN STD_LOGIC;
+	    signal_detect : IN STD_LOGIC;
+	    gt0_qplloutclk_out : OUT STD_LOGIC;
+	    gt0_qplloutrefclk_out : OUT STD_LOGIC;
+	    gt0_rxchariscomma_out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+	    gt0_rxcharisk_out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+	    gt0_rxbyteisaligned_out : OUT STD_LOGIC;
+	    gt0_rxbyterealign_out : OUT STD_LOGIC;
+	    gt0_rxcommadet_out : OUT STD_LOGIC;
+	    gt0_txpolarity_in : IN STD_LOGIC;
+	    gt0_txdiffctrl_in : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+	    gt0_txpostcursor_in : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+	    gt0_txprecursor_in : IN STD_LOGIC_VECTOR(4 DOWNTO 0);
+	    gt0_rxpolarity_in : IN STD_LOGIC;
+	    gt0_txprbssel_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+	    gt0_txprbsforceerr_in : IN STD_LOGIC;
+	    gt0_rxprbscntreset_in : IN STD_LOGIC;
+	    gt0_rxprbserr_out : OUT STD_LOGIC;
+	    gt0_rxprbssel_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+	    gt0_loopback_in : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
+	    gt0_txresetdone_out : OUT STD_LOGIC;
+	    gt0_rxresetdone_out : OUT STD_LOGIC;
+	    gt0_rxpmaresetdone_out : OUT STD_LOGIC;
+	    gt0_rxdisperr_out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+	    gt0_txbufstatus_out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+	    gt0_rxnotintable_out : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+	    gt0_eyescanreset_in : IN STD_LOGIC;
+	    gt0_eyescandataerror_out : OUT STD_LOGIC;
+	    gt0_eyescantrigger_in : IN STD_LOGIC;
+	    gt0_rxcdrhold_in : IN STD_LOGIC;
+	    gt0_rxpmareset_in : IN STD_LOGIC;
+	    gt0_txpmareset_in : IN STD_LOGIC;
+	    gt0_rxpcsreset_in : IN STD_LOGIC;
+	    gt0_txpcsreset_in : IN STD_LOGIC;
+	    gt0_rxbufreset_in : IN STD_LOGIC;
+	    gt0_rxbufstatus_out : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
+	    gt0_rxdfelpmreset_in : IN STD_LOGIC;
+	    gt0_rxdfeagcovrden_in : IN STD_LOGIC;
+	    gt0_rxlpmen_in : IN STD_LOGIC;
+	    gt0_rxmonitorout_out : OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
+	    gt0_rxmonitorsel_in : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+	    gt0_drpaddr_in : IN STD_LOGIC_VECTOR(8 DOWNTO 0);
+	    gt0_drpclk_in : IN STD_LOGIC;
+	    gt0_drpdi_in : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+	    gt0_drpdo_out : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+	    gt0_drpen_in : IN STD_LOGIC;
+	    gt0_drprdy_out : OUT STD_LOGIC;
+	    gt0_drpwe_in : IN STD_LOGIC;
+	    gt0_dmonitorout_out : OUT STD_LOGIC_VECTOR(16 DOWNTO 0)
+	  );
+	END COMPONENT;
+	ATTRIBUTE SYN_BLACK_BOX : BOOLEAN;
+	ATTRIBUTE SYN_BLACK_BOX OF gig_ethernet_pcs_pma_0 : COMPONENT IS TRUE;
+	ATTRIBUTE BLACK_BOX_PAD_PIN : STRING;
+	ATTRIBUTE BLACK_BOX_PAD_PIN OF gig_ethernet_pcs_pma_0 : COMPONENT IS "gtrefclk_p,gtrefclk_n,gtrefclk_out,txn,txp,rxn,rxp,independent_clock_bufg,userclk_out,userclk2_out,rxuserclk_out,rxuserclk2_out,resetdone,pma_reset_out,mmcm_locked_out,gmii_txd[7:0],gmii_tx_en,gmii_tx_er,gmii_rxd[7:0],gmii_rx_dv,gmii_rx_er,gmii_isolate,configuration_vector[4:0],status_vector[15:0],reset,signal_detect,gt0_qplloutclk_out,gt0_qplloutrefclk_out,gt0_rxchariscomma_out[1:0],gt0_rxcharisk_out[1:0],gt0_rxbyteisaligned_out,gt0_rxbyterealign_out,gt0_rxcommadet_out,gt0_txpolarity_in,gt0_txdiffctrl_in[3:0],gt0_txpostcursor_in[4:0],gt0_txprecursor_in[4:0],gt0_rxpolarity_in,gt0_txprbssel_in[2:0],gt0_txprbsforceerr_in,gt0_rxprbscntreset_in,gt0_rxprbserr_out,gt0_rxprbssel_in[2:0],gt0_loopback_in[2:0],gt0_txresetdone_out,gt0_rxresetdone_out,gt0_rxpmaresetdone_out,gt0_rxdisperr_out[1:0],gt0_txbufstatus_out[1:0],gt0_rxnotintable_out[1:0],gt0_eyescanreset_in,gt0_eyescandataerror_out,gt0_eyescantrigger_in,gt0_rxcdrhold_in,gt0_rxpmareset_in,gt0_txpmareset_in,gt0_rxpcsreset_in,gt0_txpcsreset_in,gt0_rxbufreset_in,gt0_rxbufstatus_out[2:0],gt0_rxdfelpmreset_in,gt0_rxdfeagcovrden_in,gt0_rxlpmen_in,gt0_rxmonitorout_out[6:0],gt0_rxmonitorsel_in[1:0],gt0_drpaddr_in[8:0],gt0_drpclk_in,gt0_drpdi_in[15:0],gt0_drpdo_out[15:0],gt0_drpen_in,gt0_drprdy_out,gt0_drpwe_in,gt0_dmonitorout_out[16:0]";
 	
 	signal gmii_txd, gmii_rxd: std_logic_vector(7 downto 0);
 	signal gmii_tx_en, gmii_tx_er, gmii_rx_dv, gmii_rx_er: std_logic;
@@ -177,7 +222,57 @@ begin
 			configuration_vector => phy_config,
 			status_vector => status,
 			reset => rsti,
-			signal_detect => sig_det
-		);
+			signal_detect => sig_det,
+			-- WFD1 has some pins accidentally swapped, so we need to reverse the polarity
+			gt0_txpolarity_in => '1', 
+			gt0_rxpolarity_in => '1',
+
+			-- other low-level stuff that comes along with the polarity
+			-- set to 0 per IP generated with this stuff turned off
+			gt0_rxchariscomma_out => open,
+		    gt0_rxcharisk_out => open,
+		    gt0_rxbyteisaligned_out => open,
+		    gt0_rxbyterealign_out => open,
+		    gt0_rxcommadet_out => open,
+		    gt0_txdiffctrl_in => (others=>'0'),
+		    gt0_txpostcursor_in => (others=>'0'),
+		    gt0_txprecursor_in => (others=>'0'),
+		    gt0_txprbssel_in => (others=>'0'),
+		    gt0_txprbsforceerr_in => '0',
+		    gt0_rxprbscntreset_in => '0',
+		    gt0_rxprbserr_out => open,
+		    gt0_rxprbssel_in => (others=>'0'),
+		    gt0_loopback_in => (others=>'0'),
+		    gt0_txresetdone_out => open,
+		    gt0_rxresetdone_out => open,
+		    gt0_rxpmaresetdone_out => open,
+		    gt0_rxdisperr_out => open,
+		    gt0_txbufstatus_out => open,
+		    gt0_rxnotintable_out => open,
+		    gt0_eyescanreset_in => '0',
+		    gt0_eyescandataerror_out => open,
+		    gt0_eyescantrigger_in => '0',
+		    gt0_rxcdrhold_in => '0',
+		    gt0_rxpmareset_in => '0',
+		    gt0_txpmareset_in => '0',
+		    gt0_rxpcsreset_in => '0',
+		    gt0_txpcsreset_in => '0',
+		    gt0_rxbufreset_in => '0',
+		    gt0_rxbufstatus_out => open,
+		    gt0_rxdfelpmreset_in => '0',
+		    gt0_rxdfeagcovrden_in => '0',
+		    gt0_rxlpmen_in => '0',
+		    gt0_rxmonitorout_out => open,
+		    gt0_rxmonitorsel_in => (others=>'0'),
+		    gt0_drpaddr_in => (others=>'0'),
+		    gt0_drpclk_in => '0',
+		    gt0_drpdi_in => (others=>'0'),
+		    gt0_drpdo_out => open,
+		    gt0_drpen_in => '0',
+
+		    gt0_drprdy_out => open,
+		    gt0_drpwe_in => '0',
+		    gt0_dmonitorout_out => open
+			);
 
 end rtl;
--- scripts/read_ip.tcl
+++ scripts/read_ip.tcl
@@ -6,10 +6,12 @@ set_property part xc7k160tfbg676-2 [current_project]
 if {[file exists $ROOT/ip/gig_ethernet_pcs_pma_0/gig_ethernet_pcs_pma_0.xci]} {
 	read_ip $ROOT/ip/gig_ethernet_pcs_pma_0/gig_ethernet_pcs_pma_0.xci
 } else {
+	set_property target_language VHDL [current_project]
 	create_ip -name gig_ethernet_pcs_pma -vendor xilinx.com -library ip -module_name gig_ethernet_pcs_pma_0 -dir $ROOT/ip
-	set_property -dict [list CONFIG.SupportLevel {Include_Shared_Logic_in_Core} CONFIG.TransceiverControl {false} CONFIG.Management_Interface {false} CONFIG.Auto_Negotiation {false}] [get_ips gig_ethernet_pcs_pma_0]
+	set_property -dict [list CONFIG.SupportLevel {Include_Shared_Logic_in_Core} CONFIG.TransceiverControl {true} CONFIG.Management_Interface {false} CONFIG.Auto_Negotiation {false}] [get_ips gig_ethernet_pcs_pma_0]
 	generate_target all [get_files $ROOT/ip/gig_ethernet_pcs_pma_0/gig_ethernet_pcs_pma_0.xci]
 	synth_ip [get_ips gig_ethernet_pcs_pma_0]
+	set_property target_language Verilog [current_project]
 }
 
 
