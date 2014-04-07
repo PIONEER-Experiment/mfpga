5e37097667a35ffdc0052b3b2c21032e65703749
Nic Eggert <nse23@cornell.edu>
2014-04-07 11:38:06 -0400

    Working on AXI loopback. WIP

--- scripts/read_ip.tcl
+++ scripts/read_ip.tcl
@@ -18,3 +18,14 @@ if {[file exists $ROOT/ip/gig_ethernet_pcs_pma_0/gig_ethernet_pcs_pma_0.xci]} {
 # disable IP constraints, since we want to do it ourselves
 
 set_property is_enabled false [get_files $ROOT/ip/gig_ethernet_pcs_pma_0/synth/gig_ethernet_pcs_pma_0_ooc.xdc]
+
+# AXI4-Stream Data FIFO
+if {[file exists $ROOT/ip/axis_data_fifo_ipbus_loopback/axis_data_fifo_ipbus_loopback.xci]} {
+	read_ip $ROOT/ip/axis_data_fifo_ipbus_loopback/axis_data_fifo_ipbus_loopback.xci
+} else {
+	set_property target_language Verilog [current_project]
+	create_ip -name axis_data_fifo -vendor xilinx.com -library ip -module_name axis_data_fifo_ipbus_loopback -dir $ROOT/ip
+	set_property -dict [list CONFIG.TDATA_NUM_BYTES {4} CONFIG.TID_WIDTH {4} CONFIG.TDEST_WIDTH {4} CONFIG.FIFO_DEPTH {128} CONFIG.HAS_TKEEP {1} CONFIG.HAS_TLAST {1}] [get_ips axis_data_fifo_ipbus_loopback]
+	generate_target all [get_files $ROOT/ip/axis_data_fifo_ipbus_loopback/axis_data_fifo_ipbus_loopback.xci]
+	synth_ip [get_ips axis_data_fifo_ipbus_loopback]
+}
