dcfe8aeee3cbdf6fb572356015d4ad4e544a87df
Nic Eggert <nse23@cornell.edu>
2014-05-30 13:10:50 -0400

    Add implement_only

---
+++ scripts/implement_only.tcl
@@ -0,0 +1,9 @@
+opt_design
+# power_opt_design
+place_design
+phys_opt_design
+route_design
+
+write_bitstream -force $ROOT/bitstreams/wfd_master.bit
+write_debug_probes -force $ROOT/bitstreams/debug.ltx
+
