0b69332f0b0c3764b742e783155b7ac8cc87396b
David Sweigart <das556@cornell.edu>
2015-03-10 10:55:13 -0400

    Fixed debug LTX filename in export Tcl script.

--- scripts/export_bitstream.tcl
+++ scripts/export_bitstream.tcl
@@ -7,9 +7,9 @@ if {[file exists ./wfd_top.bit]} {
 }
 
 # Export the debug file
-if {[file exists ./debug_netlist.ltx]} {
-  file copy -force ./debug_netlist.ltx [file dirname [info script]]/../debugs/debug.ltx
+if {[file exists ./debug_nets.ltx]} {
+  file copy -force ./debug_nets.ltx [file dirname [info script]]/../debugs/debug.ltx
   puts "INFO: Debug copied: debug.ltx"
 } else {
-  puts "ERROR: Debug not found: debug_netlist.ltx"
+  puts "ERROR: Debug not found: debug_nets.ltx"
 }
