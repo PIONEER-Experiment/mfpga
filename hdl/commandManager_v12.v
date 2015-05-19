09304129709d735729854344f8877cd7ce863f33
David Sweigart <das556@cornell.edu>
2015-05-19 11:38:39 -0400

    Modified commandManager for stress test: change channel header, add channel checksum in data stream.

--- hdl/commandManager_v12.v
+++ hdl/commandManager_v12.v
@@ -2,29 +2,29 @@
 // Created by fizzim.pl version $Revision: 5.0 on 2015:03:31 at 10:55:00 (www.fizzim.com)
 
 module commandManager (
-  output reg busy,
+  (* mark_debug = "true" *) output reg busy,
   output reg chan_rx_fifo_ready,
   output reg [31:0] chan_tx_fifo_data,
   output reg [3:0] chan_tx_fifo_dest,
   output reg chan_tx_fifo_last,
   output reg chan_tx_fifo_valid,
-  output reg [63:0] daq_data,
-  output reg daq_header,
-  output reg daq_trailer,
-  output reg daq_valid,
+  (* mark_debug = "true" *) output reg [63:0] daq_data,
+  (* mark_debug = "true" *) output reg daq_header,
+  (* mark_debug = "true" *) output reg daq_trailer,
+  (* mark_debug = "true" *) output reg daq_valid,
   output reg ipbus_cmd_ready,
   output reg [31:0] ipbus_res_data,
   output reg ipbus_res_last,
   output reg ipbus_res_valid,
   output reg read_fill_done,
-  output reg tm_fifo_ready,
+  (* mark_debug = "true" *) output reg tm_fifo_ready,
   input wire [4:0] chan_en,
-  input wire [31:0] chan_rx_fifo_data,
+  (* mark_debug = "true" *) input wire [31:0] chan_rx_fifo_data,
   input wire chan_rx_fifo_last,
   input wire chan_rx_fifo_valid,
   input wire chan_tx_fifo_ready,
-  input wire clk,
-  input wire daq_ready,
+  (* mark_debug = "true" *) input wire clk,
+  (* mark_debug = "true" *) input wire daq_ready,
   input wire [31:0] ipbus_cmd_data,
   input wire [3:0] ipbus_cmd_dest,
   input wire ipbus_cmd_last,
@@ -64,21 +64,22 @@ module commandManager (
   SEND_IPBUS_CMD              = 24, 
   SEND_IPBUS_CSN              = 25, 
   SEND_IPBUS_RES              = 26, 
-  STORE_CHAN_TAG_AND_FILLTYPE = 27; 
+  STORE_CHAN_TAG_AND_FILLTYPE = 27, 
+  SEND_CHAN_HEADER2           = 28; 
 
-  reg [27:0] state;
-  reg [31:0] burst_count;
-  reg [31:0] chan_num_buf;
-  reg [31:0] chan_tag_filltype;
-  reg [31:0] csn;
-  reg [31:0] data_count;
-  reg [31:0] ddr_start_addr;
-  reg [31:0] ipbus_buf;
-  reg [2:0] num_chan_en;
-  reg sent_header;
-  reg [31:0] trig_num_buf;
+  (* mark_debug = "true" *) reg [28:0] state;
+  (* mark_debug = "true" *) reg [31:0] burst_count;
+  (* mark_debug = "true" *) reg [31:0] chan_num_buf;
+  (* mark_debug = "true" *) reg [31:0] chan_tag_filltype;
+  (* mark_debug = "true" *) reg [31:0] csn;
+  (* mark_debug = "true" *) reg [31:0] data_count;
+  (* mark_debug = "true" *) reg [31:0] ddr_start_addr;
+  (* mark_debug = "true" *) reg [31:0] ipbus_buf;
+  (* mark_debug = "true" *) reg [2:0] num_chan_en;
+  (* mark_debug = "true" *) reg sent_header;
+  (* mark_debug = "true" *) reg [31:0] trig_num_buf;
   
-  reg [27:0] nextstate;
+  reg [28:0] nextstate;
   reg [31:0] next_burst_count;
   reg [31:0] next_chan_num_buf;
   reg [31:0] next_chan_tag_filltype;
@@ -96,7 +97,7 @@ module commandManager (
 
   // comb always block
   always @* begin
-    nextstate = 28'b0000000000000000000000000000;
+    nextstate = 29'b00000000000000000000000000000;
     chan_tx_fifo_data[31:0] = 0; // default
     ipbus_res_data[31:0] = 0; // default
     next_burst_count[31:0] = burst_count[31:0];
@@ -157,13 +158,14 @@ module commandManager (
         begin
           nextstate[SEND_AMC13_TRAILER] = 1'b1;
           // *2 = 2 64-bit words per burst
-          // +1 = 1 channel header per channel data set
+          // +2 = 2 64-bit channel header words per channel data set
+          // +2 = 2 64-bit channel checksum words per channel data set
           // *num_chan_en = # channels that will send data
           // +3 = 3 (2 AMC13 header + 1 AMC13 trailer) 64-bit words per AMC13 data set
 
           // burst_count[19:0]   = # 128-bit data words
           // burst_count[19:0]*2 = # 64-bit data words
-          next_daq_data[63:0] = {32'h00000000,{{trig_num_buf[7:0],4'h0},(burst_count[19:0]*2+1)*num_chan_en+3}};
+          next_daq_data[63:0] = {32'h00000000,{{trig_num_buf[7:0],4'h0},(burst_count[19:0]*2+2+2)*num_chan_en+3}};
         end
       end
       state[READ_CHAN_BURST_COUNT]      : begin
@@ -179,7 +181,7 @@ module commandManager (
         if (chan_rx_fifo_valid) begin
           nextstate[READ_CHAN_DATA2] = 1'b1;
           next_data_count[31:0] = data_count[31:0]+1;
-          next_daq_data[63:0] = {chan_rx_fifo_data[31:0],32'h00000000};
+          next_daq_data[63:0] = {32'h00000000,chan_rx_fifo_data[31:0]};
         end
         else begin
           nextstate[READ_CHAN_DATA1] = 1'b1; // Added because implied_loopback is true
@@ -189,7 +191,7 @@ module commandManager (
         if (chan_rx_fifo_valid) begin
           nextstate[SEND_CHAN_DATA] = 1'b1;
           next_data_count[31:0] = data_count[31:0]+1;
-          next_daq_data[63:0] = {daq_data[63:32],chan_rx_fifo_data[31:0]};
+          next_daq_data[63:0] = {chan_rx_fifo_data[31:0],daq_data[31:0]};
         end
         else begin
           nextstate[READ_CHAN_DATA2] = 1'b1; // Added because implied_loopback is true
@@ -239,15 +241,17 @@ module commandManager (
         end
       end
       state[READ_CHECKSUM]              : begin
-        if (chan_rx_fifo_valid && chan_rx_fifo_last) begin
+        // commented out 'nextstate' logic: will automatically go to CHECK_CHAN_EN
+        // this state doesn't do anything any longer since we're sending the checksum as data now
+        //if (chan_rx_fifo_valid && chan_rx_fifo_last) begin
           nextstate[CHECK_CHAN_EN] = 1'b1;
           next_chan_tx_fifo_dest[3:0] = chan_tx_fifo_dest[3:0]+1;
           next_daq_data[63:0] = 0;
           next_csn[31:0] = csn[31:0]+1;
-        end
-        else begin
-          nextstate[READ_CHECKSUM] = 1'b1; // Added because implied_loopback is true
-        end
+        //end
+        //else begin
+        //  nextstate[READ_CHECKSUM] = 1'b1; // Added because implied_loopback is true
+        //end
       end
       state[READ_IPBUS_CMD]             : begin
         if (ipbus_cmd_valid) begin
@@ -288,7 +292,7 @@ module commandManager (
       state[SEND_AMC13_HEADER2]         : begin
         if (daq_ready) begin
           nextstate[SEND_CHAN_HEADER] = 1'b1;
-          next_daq_data[63:0] = {chan_tag_filltype[17:0],{trig_num_buf[23:0],burst_count[21:0]}};
+          next_daq_data[63:0] = {{{2'b01,12'b0},chan_tag_filltype[17:0]},{11'b0,burst_count[20:0]}};
           next_sent_header = 1;
         end
         else begin
@@ -330,7 +334,8 @@ module commandManager (
         end
       end
       state[SEND_CHAN_DATA]             : begin
-        if (daq_ready && data_count[31:0] == burst_count[31:0]*4) begin
+        // extra '+4' is for sending the channel checksum along with the data
+        if (daq_ready && (data_count[31:0] == (burst_count[31:0]*4+4))) begin
           nextstate[READ_CHECKSUM] = 1'b1;
         end
         else if (daq_ready) begin
@@ -343,11 +348,20 @@ module commandManager (
       end
       state[SEND_CHAN_HEADER]           : begin
         if (daq_ready) begin
+          nextstate[SEND_CHAN_HEADER2] = 1'b1;
+          next_daq_data[63:0] = {{6'b0,{ddr_start_addr[25:3],3'b0}},{8'b0,trig_num_buf[23:0]}};
+        end
+        else begin
+          nextstate[SEND_CHAN_HEADER] = 1'b1; // Added because implied_loopback is true
+        end
+      end
+      state[SEND_CHAN_HEADER2]           : begin
+        if (daq_ready) begin
           nextstate[READ_CHAN_DATA1] = 1'b1;
           next_data_count[31:0] = 0;
         end
         else begin
-          nextstate[SEND_CHAN_HEADER] = 1'b1; // Added because implied_loopback is true
+          nextstate[SEND_CHAN_HEADER2] = 1'b1; // Added because implied_loopback is true
         end
       end
       state[SEND_IPBUS_CMD]             : begin
@@ -392,7 +406,7 @@ module commandManager (
         end
         else begin
           nextstate[SEND_CHAN_HEADER] = 1'b1;
-          next_daq_data[63:0] = {chan_tag_filltype[17:0],{trig_num_buf[23:0],burst_count[21:0]}};
+          next_daq_data[63:0] = {{{2'b01,12'b0},chan_tag_filltype[17:0]},{11'b0,burst_count[20:0]}};
         end
       end
     endcase
@@ -537,6 +551,9 @@ module commandManager (
         nextstate[SEND_CHAN_HEADER]           : begin
           daq_valid <= 1;
         end
+        nextstate[SEND_CHAN_HEADER2]          : begin
+          daq_valid <= 1;
+        end
         nextstate[SEND_IPBUS_CMD]             : begin
           chan_tx_fifo_valid <= 1;
         end
@@ -606,6 +623,8 @@ module commandManager (
         statename = "SEND_CHAN_DATA";
       state[SEND_CHAN_HEADER]           :
         statename = "SEND_CHAN_HEADER";
+      state[SEND_CHAN_HEADER2]          :
+        statename = "SEND_CHAN_HEADER2";
       state[SEND_IPBUS_CMD]             :
         statename = "SEND_IPBUS_CMD";
       state[SEND_IPBUS_CSN]             :
