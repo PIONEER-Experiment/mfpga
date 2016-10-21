44271c2b424070d5ff8d20c8d1eef059b1208877
David Sweigart <das556@cornell.edu>
2016-10-21 16:55:25 -0500

    [2.0.0] Updated slave register assignments to match Master v2.1.0.

--- ipbus/hdl/slaves.vhd
+++ ipbus/hdl/slaves.vhd
@@ -159,36 +159,24 @@ begin
 		);
 
 		-- control register
-		rst_out               <= ctrl_reg(0);
-		chan_done_out(0)      <= ctrl_reg(1);
-		chan_done_out(1)      <= ctrl_reg(2);
-		chan_done_out(2)      <= ctrl_reg(3);
-		chan_done_out(3)      <= ctrl_reg(4);
-		chan_done_out(4)      <= ctrl_reg(5);
-		chan_en_out(0)        <= ctrl_reg(6);
-		chan_en_out(1)        <= ctrl_reg(7);
-		chan_en_out(2)        <= ctrl_reg(8);
-		chan_en_out(3)        <= ctrl_reg(9);
-		chan_en_out(4)        <= ctrl_reg(10);
-		prog_chan_out         <= ctrl_reg(11);
-		reprog_trigger_out(0) <= ctrl_reg(12);
-		reprog_trigger_out(1) <= ctrl_reg(13);
-        trig_delay_out(0)     <= ctrl_reg(14);
-        trig_delay_out(1)     <= ctrl_reg(15);
-        trig_delay_out(2)     <= ctrl_reg(16);
-        trig_delay_out(3)     <= ctrl_reg(17);
-        endianness_out        <= ctrl_reg(18);
-        trig_settings_out(0)  <= ctrl_reg(19);
-        trig_settings_out(1)  <= ctrl_reg(20);
-        trig_settings_out(2)  <= ctrl_reg(21);
-        trig_settings_out(3)  <= ctrl_reg(22);
-        trig_settings_out(4)  <= ctrl_reg(23);
-        trig_settings_out(5)  <= ctrl_reg(24);
-        trig_settings_out(6)  <= ctrl_reg(25);
-        trig_settings_out(7)  <= ctrl_reg(26);
-        trig_sel_out(0)       <= ctrl_reg(27);
-        trig_sel_out(1)       <= ctrl_reg(28);
-        ttc_loopback_out      <= ctrl_reg(29);
+		rst_out               <= ctrl_reg( 0);
+		-- async_mode_out        <= ctrl_reg( 1);
+		prog_chan_out         <= ctrl_reg( 2);
+		reprog_trigger_out(0) <= ctrl_reg( 3);
+		reprog_trigger_out(1) <= ctrl_reg( 4);
+		chan_en_out(0)        <= ctrl_reg( 5);
+		chan_en_out(1)        <= ctrl_reg( 6);
+		chan_en_out(2)        <= ctrl_reg( 7);
+		chan_en_out(3)        <= ctrl_reg( 8);
+		chan_en_out(4)        <= ctrl_reg( 9);
+        endianness_out        <= ctrl_reg(10);
+        trig_settings_out(0)  <= ctrl_reg(11);
+        trig_settings_out(1)  <= ctrl_reg(12);
+        trig_settings_out(2)  <= ctrl_reg(13);
+        ttc_loopback_out      <= ctrl_reg(14);
+        -- ext_trig_pulse_en_out <= ctrl_reg(15);
+        -- async_trig_type_out   <= ctrl_reg(16);
+    	-- accept_pulse_trig_out <= ctrl_reg(17);
 
 -- Slave 2: Write-only register
 
