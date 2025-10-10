b393d7797c46cc69c91292f92e2eebf178070d6a
LawrenceGibbons <lkg5@cornell.edu>
2025-10-10 10:37:37 -0400

    Bug-fixed first working version of the circular-buffered self triggering mode v6.1.0

---
+++ scripts/export_bitstream_selftrigc.tcl
@@ -0,0 +1,17 @@
+# Export the bitstream file and create the .mcs file
+if {[file exists ./wfd_selftrig_top.bit]} {
+  file copy -force ./wfd_selftrig_top.bit [file dirname [info script]]/../bitstreams/wfd_master_selftrig_$version.bit
+  puts "INFO: Bitstream copied: wfd_master.bit"
+  write_cfgmem -force -format MCS -size 32 -interface SPIx1 \
+      -loadbit "up 0x00670000 ./wfd_selftrig_top.bit" [file dirname [info script]]/../bitstreams/wfd_master_selftrig_$version
+} else {
+  puts "ERROR: Bitstream not found: wfd_selftrig_top.bit"
+}
+
+# Export the debug file
+if {[file exists ./debug_nets.ltx]} {
+  file copy -force ./debug_nets.ltx [file dirname [info script]]/../bitstreams/wfd_master_selftrig_$version.ltx
+  puts "INFO: Debug copied: wfd_master.ltx"
+} else {
+  puts "ERROR: Debug not found: debug_nets.ltx"
+}
